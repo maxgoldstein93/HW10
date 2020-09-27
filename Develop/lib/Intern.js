45bdf879698332bcb57d978b2e55aabe7951b722
Max Goldstein <magoldstein93@gmail.com>
2020-09-27 16:28:55 -0400

    working test for manager and intern

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+function Intern (name, id, email, school) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.school = school;
+    this.getName = () =>{
+        return this.name;
+    }
+    this.getId = () =>{
+        return this.id;
+    }
+    this.getEmail = () => {
+        return this.email;
+    }
+    this.getRole = () => {
+        return "Intern"
+    }
+    this.getSchool = () => {
+        return this.school;
+    }
+}
+
+module.exports = Intern;
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,24 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+function Manager (name, id, email, officeNumber) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.officeNumber = officeNumber;
+    this.getName = () =>{
+        return this.name;
+    }
+    this.getId = () =>{
+        return this.id;
+    }
+    this.getEmail = () => {
+        return this.email;
+    }
+    this.getOfficeNumber = () => {
+        return this.officeNumber;
+    }
+    this.getRole = () => {
+        return "Manager"
+    }
+}
+
+module.exports = Manager;
