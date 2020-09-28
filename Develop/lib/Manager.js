aa66643bb077f78ba4d2a38a4f9a9f2dab439933
Max Goldstein <magoldstein93@gmail.com>
2020-09-28 15:40:47 -0400

    removed repeated code in Manager

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -6,15 +6,6 @@ class Manager extends Employee {
         super(name, id, email)
         this.officeNumber = officeNumber;
     }
-    getName() {
-        return this.name;
-    }
-    getId() {
-        return this.id;
-    }
-    getEmail() {
-        return this.email;
-    }
     getOfficeNumber() {
         return this.officeNumber;
     }
