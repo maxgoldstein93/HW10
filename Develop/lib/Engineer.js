65afebe981407d2900536ab3e55b2e5bb1600d92
Max Goldstein <magoldstein93@gmail.com>
2020-09-28 15:43:38 -0400

    removef repeated code Engineer

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -6,15 +6,6 @@ class Engineer extends Employee{
         super(name,id,email)
         this.github = github
     }
-        getName(){
-            return this.name;
-        }
-        getId() {
-            return this.id;
-        }
-        getEmail(){
-            return this.email;
-        }
        getGithub (){
             return this.github;
         }
